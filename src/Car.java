6ddb5ff8ec4838cae637382196631cce097aabed
Hanna Lata <annalats32@gmail.com>
2023-10-02 16:10:47 +0300

    Add class Car and its methods

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startElectricity() {
+    }
+}
