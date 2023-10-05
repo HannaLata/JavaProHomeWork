98d8987cd02ae610f5e5e023daca85690fab535a
Hanna Lata <annalats32@gmail.com>
2023-10-05 20:22:46 +0300

    implement class Solution with static methods to find anagram, add related logic to Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(Solution.isAnagramFirstSolution("anagram", "nagaram"));
+        System.out.println(Solution.isAnagramFirstSolution("rat", "car"));
+        System.out.println(Solution.isAnagramSecondSolution("anagram", "nagaram"));
+        System.out.println(Solution.isAnagramSecondSolution("rat", "car"));
     }
 }
