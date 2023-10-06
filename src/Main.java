f9a6201b2ac40524be639b863375d9d4156d3090
Hanna Lata <annalats32@gmail.com>
2023-10-06 10:41:04 +0300

    implement new objects creation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+
+import animals.Animal;
+import animals.Cat;
+import animals.Dog;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        createNewAnimals();
+    }
+
+    private static void createNewAnimals() {
+        Dog barsik = new Dog("Barsik");
+        Cat tom = new Cat("Tom");
+        barsik.run(400);
+        barsik.swim(400);
+        tom.run(150);
+        tom.swim(10);
+        Cat masya = new Cat("Masya");
+        masya.run(200);
+        masya.swim(1);
+
+        System.out.println("Number of animals created: " + Animal.countAnimals());
     }
 }
