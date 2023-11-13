0bc45fa8e6da0e8112bfb819e466c58673f70b0a
Hanna Lata <annalats32@gmail.com>
2023-11-13 17:29:26 +0200

    implement ArrayValueCalculator class, create custom exceptions

---
+++ src/exceptions/ArrayDataException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ArrayDataException extends Exception {
+    public ArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
